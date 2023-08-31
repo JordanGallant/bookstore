e83f35fc4963af36ac5902988b3d4dc313b8bc34
JordanGallant <39-12602@sae.edu.za>
2023-08-31 07:21:07 +0200

    delete-implementation

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,8 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -28,7 +28,12 @@ app.get("/books", (req, res) => {
 
 app.post("/books", (req, res) => {
   const q = "INSERT INTO books (`title`, `desc`,`price`, `cover`) VALUES (?)";
-  const values = [req.body.title, req.body.desc,req.body.price, req.body.cover];
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
@@ -40,4 +45,12 @@ app.listen(8800, () => {
   console.log("connected to backend");
 });
 
-app.delete("/books/:id")
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted sucessfully");
+  });
+});
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,39 +1,52 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export const Books = () => {
-    const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
 
-    useEffect(()=>{
-        const fetchAllBooks = async () =>{
-            try{
-                const res = await axios.get("http://localhost:8800/books")
-                setBooks(res.data)
-            }catch(err){
-                console.log(err)
-            }
-        }
-        fetchAllBooks()
-    },[])
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+        await axios.delete("http://localhost:8800/books/" +id)
+        window.location.reload()
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
-        <h1>JG Book Shop</h1>
-        <div className="books">
-         {books.map(book=>(
-             <div className="book" key = {book.id}>
-                 {book.cover && <img src={book.cover} alt="" />}
-                 <h2>{book.title}</h2>
-                 <p>{book.desc}</p>
-                 <span>{book.price}</span>
-                 <button className='delete'>Delete</button>
-                 <button className='update'>Update</button>
-             </div>
-         ))}   
-        </div>
-        <button><Link to = "/add">Add new book</Link></button>
+      <h1>JG Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+            <button className="delete" onClick={() => handleDelete(book.id)}>
+              Delete
+            </button>
+            <button className="update">Update</button>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Add new book</Link>
+      </button>
     </div>
-  )
-}
+  );
+};
